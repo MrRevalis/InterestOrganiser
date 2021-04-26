c4b0362f6b3c239aa1f87e99e1d9970a273bdd72
MrRevalis <dominik.revalis@gmail.com>
2021-04-27 00:03:41 +0200

    login page, entry with validation, first theme

---
+++ InterestOrganiser/InterestOrganiser/InterestOrganiser/Behaviors/EmailValidation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace InterestOrganiser.Behaviors
+{
+    public class EmailValidation : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.TextChanged += OnTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.TextChanged -= OnTextChanged;
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Entry entry = sender as Entry;
+            string email = e.NewTextValue;
+            Regex emailPattern = new Regex(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$");
+
+            if (emailPattern.IsMatch(email)){
+                entry.TextColor = Color.Black;
+            }
+            else
+            {
+                entry.TextColor = Color.FromHex("cc0000");
+            }
+        }
+    }
+}
---
+++ InterestOrganiser/InterestOrganiser/InterestOrganiser/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace InterestOrganiser.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public ICommand Login { get; }
+        public ICommand Register { get; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public LoginViewModel()
+        {
+            Login = new Command(async () => await LoginMethod());
+        }
+
+        private async Task LoginMethod()
+        {
+
+        }
+
+    }
+}
